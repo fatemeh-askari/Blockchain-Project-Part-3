d3492cfd6a8bf73b69ba8833c1e8bf8242441231
M. Askari <145195116+fatemeh-askari@users.noreply.github.com>
2023-12-16 22:10:52 +0330

    create transaction

--- wallet/index.js
+++ wallet/index.js
@@ -1,6 +1,7 @@
 const {STARTING_BALANCE} = require('./../config');
 const {ec} = require('./../util');
 const cryptoHash = require('./../util/crypto-hash');
+const Transaction = require('./transaction');
 
 
 class Wallet{
@@ -12,6 +13,13 @@ class Wallet{
 
   sign(data){
     return this.keyPair.sign(cryptoHash(data));
+  }
+
+  createTransaction({recipient, amount}) {
+    if(amount > this.balance) {
+      throw new Error ('amount exceeds balance');
+    } 
+    return new Transaction({senderWallet: this, recipient, amount})
   }
 }
 
