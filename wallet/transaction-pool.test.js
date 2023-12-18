56344d89ca97300124b5a0dfe58abd851db26c0b
M. Askari <145195116+fatemeh-askari@users.noreply.github.com>
2023-12-18 22:15:36 +0330

    update transaction with api

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -3,12 +3,13 @@ const Transaction = require('./transaction');
 const Wallet = require('./index');
 
 describe('TransactionPool', () => {
-  let transactionPool, transaction;
+  let transactionPool, transaction, senderWallet;
 
   beforeEach(() => {
+    senderWallet = new Wallet();
     transactionPool = new TransactionPool();
     transaction = new Transaction({
-      senderWallet: new Wallet(),
+      senderWallet: senderWallet,
       recipient: 'test-recipient',
       amount: 50
     });
@@ -21,4 +22,11 @@ describe('TransactionPool', () => {
       expect(transactionPool.transactionMap[transaction.id]).toBe(transaction);
     })
   });
+
+  describe('existingTransaction()', () => {
+    it('returns an existing transaction given an input address', () => {
+      transactionPool.setTransaction(transaction);
+      expect(transactionPool.existingTransaction({inputAddress: senderWallet.publicKey})).toBe(transaction)
+    })
+  })
 });
