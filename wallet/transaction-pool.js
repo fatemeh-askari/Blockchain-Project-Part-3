a798160de22fc505c6703960b44e2cdc4a192761
M. Askari <145195116+fatemeh-askari@users.noreply.github.com>
2023-12-19 20:57:29 +0330

    clear transaction pool

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -21,6 +21,22 @@ class TransactionPool {
   validTransactions(){
     return Object.values(this.transactionMap).filter(transaction => Transaction.validTransaction(transaction))
   }
+
+  clear(){
+    this.transactionMap = {};
+  }
+
+  clearBlockchainTransactions({chain}){
+    for(let i=0; i < chain.length; i++){
+      const block = chain[i];
+      for(let transaction of block.data){
+        if(this.transactionMap[transaction.id]){
+          delete this.transactionMap[transaction.id]
+        }
+      }
+
+    }
+  }
 };
 
 module.exports = TransactionPool;
