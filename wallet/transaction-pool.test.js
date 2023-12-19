791fa3ed84ec049a0ada791cba42f947bf96d5ad
M. Askari <145195116+fatemeh-askari@users.noreply.github.com>
2023-12-19 19:03:12 +0330

    separation of valid transactions

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -28,5 +28,36 @@ describe('TransactionPool', () => {
       transactionPool.setTransaction(transaction);
       expect(transactionPool.existingTransaction({inputAddress: senderWallet.publicKey})).toBe(transaction)
     })
-  })
+  });
+
+
+  describe('validTransactions()', () => {
+    let validTransactions;
+
+    beforeEach(() => {
+      validTransactions = [];
+
+      for(i=0; i<10; i++){
+        transaction = new Transaction({
+          senderWallet, recipient: 'any-recipient', amount: 30
+        });
+
+        if(i%3 === 0){
+          transaction.input.amount = 999999;
+        }else if(i%3 === 1){
+          transaction.input.signature = new Wallet().sign('foo');
+        }else{
+          validTransactions.push(transaction);
+        }
+
+        transactionPool.setTransaction(transaction);
+      }
+    });
+
+    it('returns valid transactions', () => {
+      expect(transactionPool.validTransactions()).toEqual(validTransactions);
+    })
+  });
+
+
 });
